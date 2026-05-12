fb3e05643ee33f10d2f9a61d9a5ccba994ec6f5a
ankitalamichhane2004-crypto <ankitalamichhane2004@gmail.com>
2026-05-12 11:55:40 +0545

    frontend

--- practice2/app/Http/Controllers/FrontendController.php
+++ practice2/app/Http/Controllers/FrontendController.php
@@ -6,12 +6,10 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        
     }
 
     /**
